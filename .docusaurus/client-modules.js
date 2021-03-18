5822c8f7aac94e74461bf265e57898ab3d74a38e
RaiMan <sikulix@outlook.com>
2021-03-18 17:44:38 +0100

    first commit after adding the github action

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/raimundhocke/IdeaProjects/sikulix.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/raimundhocke/IdeaProjects/sikulix.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/raimundhocke/IdeaProjects/sikulix.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/raimundhocke/IdeaProjects/sikulix.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/raimundhocke/IdeaProjects/sikulix.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/raimundhocke/IdeaProjects/sikulix.github.io/src/css/custom.css"),
-];
