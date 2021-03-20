b34bd1f52a71a328aa78e6179d57e614bc6d2d34
Raimund Hocke <sikulix@outlook.com>
2021-03-20 19:51:22 +0100

    trying donate button

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,11 +127,13 @@ function Home() {
               </div>
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <div className={styles.buttons}>
                 <form action="https://www.paypal.com/donate" method="post" target="_top">
                 <input type="hidden" name="hosted_button_id" value="39VFECBWHYA4L" />
                 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
                 <img alt="" border="0" src="https://www.paypal.com/en_DE/i/scr/pixel.gif" width="1" height="1" />
                 </form>
+              </div>
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
               <div className={styles.buttons}>
