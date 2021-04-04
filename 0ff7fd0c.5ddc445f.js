2f81267db49e123ea2a4b21a7dbbe61f14a6177b
Waleed Sadek <waleed@panx.io>
2021-04-04 10:25:16 +0200

    Updating Docs

---
+++ 0ff7fd0c.5ddc445f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{72:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome to the new webpage","permalink":"/blog/welcome"}]}')}}]);
